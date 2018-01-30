a12eaf399ae7626f7115a60a07bd3d2c08aa5186
Sophia Marcus <smarcus7@gatech.edu>
2018-01-30 15:05:08 -0500

    Completed necessary method

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+        String newString = "";
+        for (int i = 0; i < input.length(); i++) {
+            newString += (char) (input.charAt(i) + 1);
+        }
+        return newString;
     }
     
     /**
