a3973fff283e427190e7d7787b47ba2085352dd4
unknown <mcraig31@gatech.edu>
2018-01-31 14:41:32 -0500

    changing my name in the main file

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("smarcus7");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("mylon");
+                  Person5 p5 = new Person5("Mylon Craig");
+                  toPrint += p5.toString("mcraig31");
 		  
         System.out.println(toPrint);
         
